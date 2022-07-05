f426cad69e1b8c6a1886739ec5956fc7ae00b8f9
Filipe Nogueira <84330960+Filipendsa@users.noreply.github.com>
2022-07-05 16:49:12 -0300

    End Identity

---
+++ src/services/NSE.Identidade.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace NSE.Identidade.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("V1", new OpenApiInfo
+                {
+                    Title = "NerdStore Enterprise Identity API",
+                    Description = "Esta API faz parte do curso ASP.NET Core Enterprise Application do desenvolvedor.io",
+                    Contact = new OpenApiContact() { Name = "Filipe Nogueira", Email = "finogs2001@gmail.com" }
+                });
+
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
--- src/services/NSE.Identidade.API/Startup.cs
+++ src/services/NSE.Identidade.API/Startup.cs
@@ -1,65 +1,46 @@
-﻿using Microsoft.EntityFrameworkCore;
-using NSE.Identidade.API.Data;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NSE.Identidade.API.Configuration;
 
 namespace NSE.Identidade.API
 {
-    public class Startup : IStartup
+    public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+
+        public Startup(IHostEnvironment hostEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            if (hostEnvironment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentityConfiguration(Configuration);
 
-            services.AddControllers();
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddApiConfiguration();
+
+            services.AddSwaggerConfiguration();
         }
-        public void Configure(WebApplication app, IWebHostEnvironment environment)
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwaggerConfiguration();
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-        }
-    }
-    public interface IStartup
-    {
-        IConfiguration Configuration { get; }
-        void Configure(WebApplication app, IWebHostEnvironment environment);
-        void ConfigureServices(IServiceCollection services);
-
-    }
-
-    public static class StartupExtensions
-    {
-        public static WebApplicationBuilder UseStartup<TStartup>(this WebApplicationBuilder WebAppBuilder) where TStartup: IStartup
-        {
-            var startup = Activator.CreateInstance(typeof(TStartup), WebAppBuilder.Configuration) as IStartup;
-            if (startup == null) throw new ArgumentException("Classe Startup.cs inválida");
-
-            startup.ConfigureServices(WebAppBuilder.Services);
-
-            var app = WebAppBuilder.Build();
-            startup.Configure(app, app.Environment);
-
-            app.Run();
-
-            return WebAppBuilder;
+            app.UseApiConfiguration(env);
         }
     }
 }
