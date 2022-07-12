7f92a705f2db1e7f6216f6d57c898d2269bd7ec9
Filipe Nogueira <84330960+Filipendsa@users.noreply.github.com>
2022-07-12 15:45:03 -0300

    Add startup and config progam -> All Services

---
+++ src/services/NSE.Cliente.API/Startup.cs
@@ -0,0 +1,77 @@
+﻿namespace NSE.Cliente.API
+{
+    public class Startup : IStartup
+    {
+        public Startup(ConfigurationManager configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostEnvironment hostEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            if (hostEnvironment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        }
+    }
+    public interface IStartup
+    {
+        IConfiguration Configuration { get; }
+        void Configure(IApplicationBuilder app, IWebHostEnvironment env);
+        void ConfigureServices(IServiceCollection services);
+    }
+    public static class StartupExtensions
+    {
+        public static WebApplicationBuilder UseStartup<TStartup>(this WebApplicationBuilder WebAppBuilder) where TStartup : IStartup
+        {
+            IStartup? startup = Activator.CreateInstance(typeof(TStartup), WebAppBuilder.Configuration) as IStartup;
+            if (startup == null) throw new ArgumentException("Classe Startup.cs inválida");
+
+            startup.ConfigureServices(WebAppBuilder.Services);
+
+            var app = WebAppBuilder.Build();
+            startup.Configure(app, app.Environment);
+
+            app.Run();
+
+            return WebAppBuilder;
+        }
+    }
+}
